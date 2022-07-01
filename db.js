6f9aa640c34c72ab63da631be2f82b65ef59ca3f
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-01 11:53:13 +0530

    add project

---
+++ db.js
@@ -0,0 +1,11 @@
+const { json } = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/crud_db',(err)=>{
+    if (!err) {
+        console.log('MongoDB connection succeeded...');
+    }else{
+        console.log('Error in DB connection : ' + JSON.stringify(err,undefined,2));
+    }
+});
+
+module.exports = mongoose;
